e3feaae503d5cc6fc3ba85539cb3392574dd817e
Seyi Adedokun <seyi.adedokun@arm.com>
2023-01-24 18:31:18 +0000

    Using Properties config to set fields' values

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/config/GameConfig.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -2,16 +2,22 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.GuessCount;
 import academy.learnprogramming.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+//@PropertySource("classpath:config/game.properties") //this is not working for me, using the below
+@PropertySource("file:${user.dir}/core/src/main/resources/config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 25;
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
 
-    private int guessCount = 8;
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
