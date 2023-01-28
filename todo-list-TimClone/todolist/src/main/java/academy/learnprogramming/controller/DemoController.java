d39f12b351e575dc6b573a7a4ab774fd141d701e
Seyi Adedokun <seyi.adedokun@arm.com>
2023-01-28 16:02:05 +0000

    Added implementation for View Resolver and View

--- todo-list-TimClone/todolist/src/main/java/academy/learnprogramming/controller/DemoController.java
+++ todo-list-TimClone/todolist/src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
+
     // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
-        return "hello Seyi";
+        return "hello";
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    // prefix + name + suffix
+    // /WEB-INF/view/welcome.jsp
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
     }
 }
