f891830cc7f1d686dc2fd5ebe1ae028fa42cf56c
Seyi Adedokun <seyi.adedokun@arm.com>
2023-02-10 18:16:34 +0000

    Added Request Interception Implementation

--- guess-the-number-game/readme.md
+++ guess-the-number-game/readme.md
@@ -1,5 +1,6 @@
 Tim used Java 10, I use Java 17
 
+Clean Install and Run WebMain class to use
 This project is implemented with java 17 and spring boot 2.7.8 and run successfully.
 It was completed in spring section (exclusive of spring boot)
 This project is later re-used in "Spring boot" section and reconfigured (majorly all the POMs, added properties file, moved Main class to package level with some content tweaks
--- guess-the-number-game/web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ guess-the-number-game/web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,8 +1,10 @@
 package academy.learnprogramming.config;
 
+import academy.learnprogramming.interceptor.RequestInterceptor;
 import academy.learnprogramming.util.ViewNames;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
@@ -21,6 +23,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 
     @Bean
